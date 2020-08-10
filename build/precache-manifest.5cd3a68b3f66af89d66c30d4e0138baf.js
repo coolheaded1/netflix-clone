3b7e9217aebdcdd577460b46c9c63de2728b4d20
coolheaded <coolheaded79@gmail.com>
2020-08-11 00:30:14 +0100

    netflix clone

---
+++ build/precache-manifest.5cd3a68b3f66af89d66c30d4e0138baf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af3e41656212dfd50faefe148e49b86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a895bd1111f903fdfb7",
+    "url": "/static/css/main.f3254162.chunk.css"
+  },
+  {
+    "revision": "7df3a8559dadb26a1f44",
+    "url": "/static/js/2.c1e14196.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.c1e14196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a895bd1111f903fdfb7",
+    "url": "/static/js/main.57092d03.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
